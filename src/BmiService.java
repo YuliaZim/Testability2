bce89920c22dfb4040af6d181ac729b158f11676
Yulia Zimina <yxalzova@gmail.com>
2025-06-05 20:03:00 +0400

    correction

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate(double a, int b) {
-        double index = b/(a*a);
+        double index = b / (a * a);
 
         return index;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double m = 1.66;
