67bd7f76512eff642cf1cd2f514d240b2440a6ca
Yulia Zimina <yxalzova@gmail.com>
2025-06-05 15:50:25 +0400

    testability2

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+    public double calculate(double a, int b) {
+        double index = b/(a*a);
+
+        return index;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double m = 1.66;
+        int kg = 64;
+        double index = service.calculate(m, kg);
+        int index1 = (int) index;
+
+        System.out.println(index1);
+
+    }
+}
